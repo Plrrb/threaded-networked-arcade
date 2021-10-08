18f8074b1355730693487186b8f05d0428885fcb
plrrb <plrrbscript@gmail.com>
2021-10-07 17:21:33 -0700

    doing these commits to sync the files

--- player1.py
+++ player1.py
@@ -79,7 +79,7 @@ def main():
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((socket.gethostname(), 5555))
-    server_socket.listen(5)
+    server_socket.listen(1)
 
     print("waiting for client connection")
     client_socket, addr = server_socket.accept()
