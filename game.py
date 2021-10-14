65d7fa78800ebad2aa7f203e685859fe24133b17
plrrb <plrrbscript@gmail.com>
2021-10-14 16:19:25 -0700

    putting both players into one file

--- game.py
+++ game.py
@@ -1,25 +1,59 @@
 import arcade
 
-from player1 import Player
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-SCREEN_TITLE = "threaded arcade"
+class Player(arcade.Sprite):
+    def __init__(self, image):
+        super().__init__(image)
+
+    def up(self):
+        self.change_y = 1
+
+    def down(self):
+        self.change_y = -1
+
+    def left(self):
+        self.change_x = -1
+
+    def right(self):
+        self.change_x = 1
+
+    def stop_x(self):
+        self.change_x = 0
+
+    def stop_y(self):
+        self.change_y = 0
 
 
 class MyGame(arcade.Window):
-    def __init__(self, width, height, title):
+    def __init__(self, width, height, title, client_socket):
         super().__init__(width, height, title)
+        self.client_socket = client_socket
         self.player1 = None
         self.player2 = None
 
     def setup(self):
         self.player1 = Player(":resources:images/topdown_tanks/tank_blue.png")
-        self.player2 = Player(":resources:images/topdown_tanks/tank_green.png")
+        self.player2 = arcade.Sprite(":resources:images/topdown_tanks/tank_red.png")
 
     def on_draw(self):
+        arcade.start_render()
+
         self.player1.draw()
         self.player2.draw()
+
+    def send_our_pos(self):
+        data = f"({self.player1.center_x}, {self.player1.center_y})".encode("ascii")
+        self.client_socket.send(data)
+
+    def recv_move(self):
+        while True:
+            player_pos = self.client_socket.recv(1024)
+            player_pos = player_pos.decode("ascii")
+
+            player_pos = eval(player_pos)
+
+            self.player2.center_x, self.player2.center_y = player_pos
+            self.send_our_pos()
 
     def on_update(self, delta_time: float):
         self.player1.update()
@@ -29,13 +63,18 @@ class MyGame(arcade.Window):
         if key == arcade.key.W:
             self.player1.up()
 
+        if key == arcade.key.A:
+            self.player1.left()
 
-def main():
-    """Main method"""
-    game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-    game.setup()
-    arcade.run()
+        if key == arcade.key.D:
+            self.player1.right()
 
+        if key == arcade.key.S:
+            self.player1.down()
 
-if __name__ == "__main__":
-    main()
+    def on_key_release(self, key, key_modifiers):
+        if key == arcade.key.W or key == arcade.key.S:
+            self.player2.stop_y()
+
+        if key == arcade.key.A or key == arcade.key.D:
+            self.player2.stop_x()
--- player1.py
+++ player1.py
@@ -2,82 +2,11 @@ import arcade
 import socket
 import threading
 
+from game import MyGame
+
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = "threaded arcade player 1"
-
-
-class Player(arcade.Sprite):
-    def __init__(self, image):
-        super().__init__(image)
-
-    def up(self):
-        self.change_y = 1
-
-    def down(self):
-        self.change_y = -1
-
-    def left(self):
-        self.change_x = -1
-
-    def right(self):
-        self.change_x = 1
-
-    def stop_x(self):
-        self.change_x = 0
-
-    def stop_y(self):
-        self.change_y = 0
-
-
-class MyGame(arcade.Window):
-    def __init__(self, width, height, title, client_socket):
-        super().__init__(width, height, title)
-        self.client_socket = client_socket
-        self.player1 = None
-        self.player2 = None
-
-    def setup(self):
-        self.player1 = Player(":resources:images/topdown_tanks/tank_blue.png")
-        self.player2 = Player(":resources:images/topdown_tanks/tank_red.png")
-        self.send_our_pos()
-
-    def on_draw(self):
-        arcade.start_render()
-
-        self.player1.draw()
-        self.player2.draw()
-
-    def send_our_pos(self):
-        data = f"({self.player1.center_x}, {self.player1.center_y})".encode("ascii")
-        self.client_socket.send(data)
-
-    def recv_move(self):
-        while True:
-            player_pos = self.client_socket.recv(1024)
-            player_pos = player_pos.decode("ascii")
-
-            player_pos = eval(player_pos)
-
-            self.player2.center_x, self.player2.center_y = player_pos
-            self.send_our_pos()
-
-    def on_update(self, delta_time: float):
-        self.player1.update()
-        self.player2.update()
-
-    def on_key_press(self, key, key_modifiers):
-        if key == arcade.key.W:
-            self.player1.up()
-
-        if key == arcade.key.A:
-            self.player1.left()
-
-        if key == arcade.key.D:
-            self.player1.right()
-
-        if key == arcade.key.S:
-            self.player1.down()
 
 
 def main():
@@ -95,6 +24,7 @@ def main():
 
     recv_thread = threading.Thread(target=game.recv_move, daemon=True)
 
+    game.send_our_pos()
     game.setup()
     recv_thread.start()
     arcade.run()
